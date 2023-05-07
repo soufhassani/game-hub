b5f5becc12ace599a592cde391b70d7fa92081f1
Soufiane <souf.hassani@gmail.com>
2023-05-07 12:30:17 +0100

    dark mode implumented

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,13 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
     >
-      <GridItem area="nav" bg="#181818">
+      <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="blue.500">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="blue.500">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
