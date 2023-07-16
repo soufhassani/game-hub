5d8184b1b25e4a6a2b497f0ad76447d47d91b2da
Soufiane <souf.hassani@gmail.com>
2023-07-16 13:18:45 +0100

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,41 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { id: "0", value: "", label: "Relevance" },
+    { id: "1", value: "-added", label: "Date added" },
+    { id: "2", value: "name", label: "Name" },
+    { id: "3", value: "-released", label: "Release date" },
+    { id: "4", value: "-metacritic", label: "Popularity" },
+    { id: "5", value: "-rating", label: "Average rating" },
+  ];
+
+  const currentSort = sortOrders.find((item) => item.value === sortOrder);
+  console.log("currentSort", currentSort);
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order by: Relevance
+        Order by: {currentSort?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Date added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average rating</MenuItem>
+        {sortOrders.map((item) => (
+          <MenuItem
+            key={item.id}
+            value={item.value}
+            onClick={() => {
+              onSelectSortOrder(item.value);
+            }}
+          >
+            {item.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
