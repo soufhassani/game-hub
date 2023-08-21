1398bcc0d6421fa3da6a88e227784f3e0858630b
Soufiane <souf.hassani@gmail.com>
2023-08-21 17:03:23 +0100

    Impliment Infinit Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
 import APIClient, { DataResponse } from "../services/api-client";
@@ -14,17 +14,21 @@ export interface Games {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<DataResponse<Games>, Error>({
+  useInfiniteQuery<DataResponse<Games>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.get({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
   });
 
 export default useGames;
